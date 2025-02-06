aa58cba0e36cac19d20f3513302eba145a7a76a5
Mubinul Islam <67111837+mubinulislam@users.noreply.github.com>
2025-02-06 20:06:31 +0600

    Update beautiful_matrix.cpp

--- beautiful_matrix.cpp
+++ beautiful_matrix.cpp
@@ -5,7 +5,6 @@ using namespace std;
 int main(){
     const int rows=5;
     const int cols=5;
-    int m,n;
     int rotation;
     int matrix[rows][cols];  //using 2D array to form a matrix from user input
 
