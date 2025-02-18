343ea56753e8e6bc995dd0c3b85e25705bdc769b
Mubinul Islam <67111837+mubinulislam@users.noreply.github.com>
2025-02-18 12:27:10 +0600

    Initial Commit
    first sorted the input.
    then compared the string side by side.
    if not equal, increase counter.

---
+++ Boy_or_GIrl.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+#include<math.h>
+
+using namespace std;
+
+int main(){
+    string input;
+    int unique=0;
+    int gender;
+    int index_min;
+    char temp;
+    cin >> input;
+
+    int n=input.length();
+
+// sorting the input string using selection sort algorithm
+    for (int i=0;i<n-1;i++){
+        index_min=i;
+
+        for (int j=i+1;j<n;j++){
+            if (input[j]<input[index_min]){
+                index_min=j;
+            }
+        }
+        if (index_min!=i){
+            temp=input[i];
+            input[i]=input[index_min];
+            input[index_min]=temp;
+        }
+    }
+    cout << input << endl;
+// after sorting we iteret through the string char by char to find the duplicates
+    for (int k=0; k<n;k++){
+        if (input[k]!=input[k+1]){
+            unique+=1;
+        }
+    }
+    cout << "unique: " << unique << endl;
+    if (unique%2!=0){
+        cout << "IGNORE HIM!";
+    }
+    else{
+        cout << "CHAT WITH HER!";
+    }
+}
