4e839777ef38987a527d3663d6b32ab7a6b967ce
Mubinul Islam <67111837+mubinulislam@users.noreply.github.com>
2025-02-21 00:28:11 +0600

    Second Commit

--- Word_Capitalization.cpp
+++ Word_Capitalization.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<cctype>
 
 using namespace std;
 
@@ -6,8 +7,9 @@ int main() {
 	string input;
 	cin >> input;
     //cout << int(input[0]);
-    if (int(input[0])<=122 && int(input[0]>=95)){
-        int(input[0]-=32);
-    }
+    //if (int(input[0])<=122 && int(input[0]>=95)){
+        //int(input[0]-=32);
+    //}
+    input[0]=toupper(input[0]);
     cout << input;
 }
