7ece11c9f6a9d22719a6ec88081204b9441ae26e
Mubinul Islam <67111837+mubinulislam@users.noreply.github.com>
2025-02-26 13:35:34 +0600

    logic simplification

--- Bear_and Big_Brother.cpp
+++ Bear_and Big_Brother.cpp
@@ -4,14 +4,14 @@ using namespace std;
 int main(){
     int a;  //Limak
     int b;  //Bob
-    int n=2;
+    //int n=2;
     int i=0;
     cin >> a >> b; /*Need to calculate when Limak is larger than Bob (a>b). 
 limaks weight is tripled and Bobs weight is doubled.*/
-    for (i=1; i<n; i++){
+    for (i=1; ; i++){
         a=a*3;
         b=b*2;
-        n+=1;
+        //n+=1;
         if (a>b){
             break;
         }
