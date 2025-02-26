81afeaad22639d4c3e76b0bffcb5ae48dc84e24b
Mubinul Islam <67111837+mubinulislam@users.noreply.github.com>
2025-02-26 16:34:01 +0600

    updated version

--- Bear_and Big_Brother.cpp
+++ Bear_and Big_Brother.cpp
@@ -8,7 +8,8 @@ int main(){
     int i=0;
     cin >> a >> b; /*Need to calculate when Limak is larger than Bob (a>b). 
 limaks weight is tripled and Bobs weight is doubled.*/
-    for (i=1; ; i++){
+    for (i=1; ;i++){ /*here condition is not given, so the for loop becomes an infinite loop.
+    no need of extra variable to count. We can just use i as counter.*/
         a=a*3;
         b=b*2;
         //n+=1;
