112a2ed2e1df2fef293813b09f7e35d7f787128a
Mubinul Islam <67111837+mubinulislam@users.noreply.github.com>
2025-02-28 21:00:01 +0600

    comment added

--- UltraFast_Mathematecian.cpp
+++ UltraFast_Mathematecian.cpp
@@ -9,15 +9,21 @@ int main(){
 
     cin >> a;
     cin >> b;
-
+    // Find the max input length dynamically.
     int bitsize= max(a.size(),b.size());
 
     // Convert string binary to bitset
     bitset<100> num1(a);
     bitset<100> num2(b);
 
+    // Perform XOR operation
     bitset<100> output = num1 ^ num2;
 
     string xor_output= output.to_string().substr(100-bitsize);
+    /*output.to_string() converts bitset<N> to a string, which has .substr().✅
+    output (which is bitset<N>) itself does not support .substr(). ❌
+    If you write output.substr(...), you'll get a compiler error because bitset doesn’t have .substr().
+    ✅ output.to_string() converts the bitset<100> to a string.
+    ✅ .substr(100 - bitsize) extracts only the required bits.*/
     cout << xor_output;
 }
