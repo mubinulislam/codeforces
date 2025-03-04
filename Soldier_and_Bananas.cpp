35ccc0054f8431bd9b47244f448f8c1839907403
Mubinul Islam <67111837+mubinulislam@users.noreply.github.com>
2025-03-04 23:33:18 +0600

    initial commit

---
+++ Soldier_and_Bananas.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<cstdlib>
+
+using namespace std;
+
+int main(){
+    long long n,k,borrow,price=0;
+    long w;
+
+    cin >> k >> w >> n;
+
+    for (int i=1; i<=n; i++){
+        price+=k*i;
+    }
+    borrow=price-w;
+    if (borrow<0){
+        borrow=0;
+        cout << borrow;
+    }
+    else{
+        cout << borrow;
+    }
+}
