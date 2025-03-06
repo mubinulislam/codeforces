52058354019eff0aaee347e1a877659d7687dca9
Mubinul Islam <67111837+mubinulislam@users.noreply.github.com>
2025-03-06 16:37:24 +0600

    initial commit

---
+++ Wrong_Subtraction.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    long long n;
+    int k;
+
+    cin >> n >> k;
+
+    for(int i=0;i<k;i++){
+        if (n%10==0){
+            n=n/10;
+        }
+        else{
+            n-=1;
+        }
+    }
+    cout << n;
+}
